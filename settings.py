da93c9cad3a5dc16372804a1eb8c36f6feab96a4
akshay <akshay@rainingclouds.com>
2012-05-11 04:18:22 +0530

    ready with first page and menu bar

--- settings.py
+++ settings.py
@@ -129,7 +129,7 @@ DEBUG_TOOLBAR_PANELS = (
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#    'django.template.loaders.eggs.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
--- urls.py
+++ urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'', include('akshaydeodotme.dashboard.urls'))
-)
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
