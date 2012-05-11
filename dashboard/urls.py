36d1d5de0f2240fe42917c211429e7713b39f22e
akshay <akshay@rainingclouds.com>
2012-05-11 13:03:00 +0530

    added projects page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+	url(r'projects$', 'akshaydeodotme.dashboard.views.projects'),
     url(r'$', 'akshaydeodotme.dashboard.views.index'),
+
 )
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
-	return render_to_response("dashboard/index.html", context_instance = RequestContext(request))
+	return render_to_response("dashboard/index.html", {'tab_id' : '1'}, context_instance = RequestContext(request))
+
+def projects(request):
+	return render_to_response("dashboard/projects.html", {'tab_id' : '2'}, context_instance = RequestContext(request))
